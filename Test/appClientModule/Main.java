08d8f528be707a448b728e87a45efcfd7749915b
RenYuXiang <mail@yuxiang.ren>
2019-11-29 10:10:28 +0800

    Update At 2019-11-29 10:10:28

---
+++ Test/appClientModule/Main.java
@@ -0,0 +1,19 @@
+
+public class Main {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double count = 1000000000;
+		for (int i = 0; i < count ; i++) {
+			System.out.println(i);
+			
+		}
+	}
+
+	/* (non-Java-doc)
+	 * @see java.lang.Object#Object()
+	 */
+	public Main() {
+		super();
+	}
+
+}
